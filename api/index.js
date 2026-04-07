04984f34243b58176dc7bb533e5be34a30648d68
Bruno Paulette <bruno.paulette@fenicio.io>
2026-04-07 11:06:39 -0300

    test

--- api/index.js
+++ api/index.js
@@ -4,9 +4,7 @@ const cors = require('cors');
 
 const app = express();
 
-app.use(cors({
-  origin: ['https://carters.com.uy']
-}));
+app.use(cors());
 app.use(express.json());
 
 const connectDB = async () => {
